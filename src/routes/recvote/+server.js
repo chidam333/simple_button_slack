ad58da58f7f8e527ac6ed6f87441a12a619f69bb
chidam <chidam3chain@gmail.com>
2024-06-11 00:47:34 +0530

    added async await"

--- src/routes/recvote/+server.js
+++ src/routes/recvote/+server.js
@@ -2,18 +2,18 @@ export async function POST({request}){
     let req = await request.json()
     console.log({req})
     // console.log({request})
-    // const {payload} = url.body;
-    // const actionPayload = JSON.parse(payload)
-    // let data = {
-    //     text: "✅ Your response recorded",
-    //     replace_original: true
-    // };
-    // fetch(actionPayload.response_url,{
-    //     method:"post",
-    //     body:JSON.stringify(data),
-    //     headers:{'Content-Type':'application/json'}
-    // }
-    // )
+    const {payload} = req;
+    const actionPayload = JSON.parse(payload)
+    let data = {
+        text: "✅ Your response recorded",
+        replace_original: true
+    };
+    fetch(actionPayload.response_url,{
+        method:"post",
+        body:JSON.stringify(data),
+        headers:{'Content-Type':'application/json'}
+    }
+    )
     // return new Response(String("ok"))
     return new Response(String(req))
 }
