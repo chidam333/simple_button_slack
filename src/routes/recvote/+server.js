2d2b8a700322be9659dbf95b8ed433f031fffd28
chidam <chidam3chain@gmail.com>
2024-06-11 00:38:36 +0530

    url -> request

--- src/routes/recvote/+server.js
+++ src/routes/recvote/+server.js
@@ -1,16 +1,18 @@
-export function POST({url}){
-    console.log({url})
-    const {payload} = url.body;
-    const actionPayload = JSON.parse(payload)
-    let data = {
-        text: "✅ Your response recorded",
-        replace_original: true
-    };
-    fetch(actionPayload.response_url,{
-        method:"post",
-        body:JSON.stringify(data),
-        headers:{'Content-Type':'application/json'}
-    }
-    )
+export function POST({request}){
+    let req = request.json()
+    console.log({req})
+    // console.log({request})
+    // const {payload} = url.body;
+    // const actionPayload = JSON.parse(payload)
+    // let data = {
+    //     text: "✅ Your response recorded",
+    //     replace_original: true
+    // };
+    // fetch(actionPayload.response_url,{
+    //     method:"post",
+    //     body:JSON.stringify(data),
+    //     headers:{'Content-Type':'application/json'}
+    // }
+    // )
     return new Response(String("ok"))
 }
